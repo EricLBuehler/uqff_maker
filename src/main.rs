b503de366f81b3c7c43204897297dec0ed0a4b8b
Eric Buehler <ericlbuehler@gmail.com>
2025-05-26 21:16:55 -0400

    Add default template

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::path::{Path, PathBuf};
 
 use anyhow::Result;
 use clap::Parser;
-use mistralrs::{IsqType, TextModelBuilder, VisionLoaderType, VisionModelBuilder};
+use mistralrs::{IsqType, TextModelBuilder, VisionModelBuilder};
 
 // Names follow the format: [name][version]-[size]-[instruct?]-[quant].uqff
 
@@ -20,6 +20,16 @@ const QUANTIZATIONS: &[IsqType] = &[
     IsqType::F8E4M3,
 ];
 
+fn default_template(model_name: &str) -> String {
+    // Lowercase and remove the first hyphen to merge name and version
+    let mut s = model_name.to_lowercase();
+    if let Some(pos) = s.find('-') {
+        s.remove(pos);
+    }
+    // Append the placeholder and extension
+    format!("{s}-###.uqff")
+}
+
 #[derive(Parser, Debug)]
 /// The model generated is output to a directory with the same name as the model ID.
 /// For example, the model ID `username/model_name` will generate the quantizations to `model_name`.
@@ -28,9 +38,9 @@ struct Args {
     #[arg(short, long)]
     model_id: String,
 
-    /// Template filename. The `###` placeholder will be replaced by each quantization.
+    /// Template filename. If not specified, will be derived from the model ID.
     #[arg(short, long)]
-    filename: String,
+    filename: Option<String>,
 
     /// To quantize a vision model, add this flag.
     #[arg(short, long, value_enum)]
@@ -41,13 +51,19 @@ struct Args {
 async fn main() -> Result<()> {
     let args = Args::parse();
     let model_id = &args.model_id;
-    let template = &args.filename;
     let model_name = Path::new(model_id)
         .file_name()
         .and_then(|s| s.to_str())
         .unwrap_or(model_id);
     let output_dir = PathBuf::from(model_name);
     std::fs::create_dir_all(&output_dir)?;
+
+    // Determine the template: use provided filename or derive from model_name
+    let template: String = if let Some(f) = &args.filename {
+        f.clone()
+    } else {
+        default_template(model_name)
+    };
 
     println!("{}\n{model_id} \n{}", "=".repeat(20), "=".repeat(20));
 
